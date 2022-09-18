f1eed0de3913d993eafc8529e398b615c4fa76ca
DDDking1 <59219500+DDDking1@users.noreply.github.com>
2022-09-18 23:01:44 +0800

    Update GitTest.java

--- src/main/java/com/hai/git/GitTest.java
+++ src/main/java/com/hai/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello 2git");
         System.out.println("push test");
+        System.out.println("remote pull");
     }
 }
